5fe53b05e44a4310ffb686de19578813563aaae7
mtanng9 <mlt152498@gmail.com>
2024-11-14 22:24:32 -0500

    made basic express server with empty routes

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const expenseRoutes = require('./routes/expense')
+const incomeRoutes = require('./routes/income')
+const userRoutes = require('./routes/user')
+
+
+const PORT = process.env.PORT || 8080;
+
+const app = express()
+app.use(express.json())
+
+app.get("/income", incomeRoutes);
+app.get("/expenses", expenseRoutes);
+app.get("/users", userRoutes);
+
+
+app.listen(PORT, () => {
+    console.log("Sever Listen on port: ", PORT);
+})
